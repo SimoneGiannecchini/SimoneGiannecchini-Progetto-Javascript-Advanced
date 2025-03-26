caf728bf6489a03f3746b2a9819e52967ee15a18
Simone <giannecchini10@gmail.com>
2025-03-26 08:23:25 +0100

    rimosso riferimento main.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/Progetto-Javascript-Advanced/', 
-})
+  base: '/SimoneGiannecchini-Progetto-Javascript-Advanced/', 
+});
